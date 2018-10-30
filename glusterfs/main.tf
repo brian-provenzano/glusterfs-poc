5c373033532224e539ec9c205f5f77ef30a80d8d
Brian Provenzano <bproven@gmail.com>
2018-10-30 10:21:34 -0600

    refactor/fix: removed local-eec provisioning from the aws_instance resource into a null resource that depends on the aws_instance resource; prevous configruation was unreliable in executing ansible setup of the cluster.  Doing provisioning in one shot seems to work best

--- glusterfs/main.tf
+++ glusterfs/main.tf
@@ -9,7 +9,6 @@ terraform {
 provider "aws" {
   version = "~> 1.9"
   region = "${var.region}"
-  #shared_credentials_file = "${var.aws_keyfile}"
   profile = "${var.aws_profilename}"
 }
 provider "template" {
@@ -29,7 +28,6 @@ provider "template" {
 module "networking" {
   source = "../modules/networking"
   
-  #inputs
   environment = "${var.environment}"
   vpc_name = "${var.vpc_name}"
   vpc_cidr = "${var.vpc_cidr}"
@@ -49,7 +47,6 @@ module "networking" {
 }
 
 # -- CREATE glusterfs servers
-
 resource "aws_instance" "glusterfs" {
 
   #number of nodes for the cluster
@@ -67,11 +64,25 @@ resource "aws_instance" "glusterfs" {
     Environment = "${var.environment}"
     GlusterNode = "true"
   }
-
+  # PULLED THE BELOW IT WAS NOT RELIABLE
   # Provision/configure the instances - have to sleep b/c we are using local-exec
   # This allows the instances to fully start up.
-    provisioner "local-exec" {
+    # provisioner "local-exec" {
+    #     working_dir = "../playbooks/"
+    #     command = "sleep 180; PRIVATEIPS=\"$(./ec2.py --profile default --list --refresh-cache | jq '._meta | {\"private_ips\":[.hostvars[].ec2_private_ip_address]}')\";ansible-playbook -b --private-key ${var.aws_keyfile} --limit ${self.public_ip} -e 'ansible_python_interpreter=/usr/bin/python3' --extra-vars \"$${PRIVATEIPS}\" gluster-cluster.yml"
+    # }
+}
+
+
+# Using null resource for provisioning after instance resrouce creation above so we can run ansible in one shot
+# It seems to work more consistently than using local-exec provisioning in the actual aws_instance resource (in my testing)
+resource "null_resource" "run_ansible_provisioning_glustercluster" {
+  # - This pulls dynamic private ip inv via ec2.py script, parses with jq to create json object and passes it to ansible as an --extra-var
+  #PURPOSE: allows us to dynamicly configure the cluster IPs in the ansible gluster_volume task in gluster-cluster.yml playbook
+  depends_on = ["aws_instance.glusterfs"]
+  count = 1 #use this with conditional to control re-run on node destruction tests.  Its hacky :(
+  provisioner "local-exec" {
         working_dir = "../playbooks/"
-        command = "sleep 180; PRIVATEIPS=\"$(./ec2.py --profile default --list --refresh-cache | jq '._meta | {\"private_ips\":[.hostvars[].ec2_private_ip_address]}')\";ansible-playbook -b --private-key ${var.aws_keyfile} --limit ${self.public_ip} -e 'ansible_python_interpreter=/usr/bin/python3' --extra-vars 'nodes=${var.nodecount}' --extra-vars 'ip=\"$${PRIVATEIPS}\"' gluster-cluster.yml"
+        command = "sleep 120; PRIVATEIPS=\"$(./ec2.py --profile default --list --refresh-cache | jq '._meta | {\"private_ips\":[.hostvars[].ec2_private_ip_address]}')\";ansible-playbook -b --private-key ${var.aws_keyfile} -e 'ansible_python_interpreter=/usr/bin/python3' --extra-vars \"$${PRIVATEIPS}\" gluster-cluster.yml"
     }
 }
