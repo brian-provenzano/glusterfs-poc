f063aa6a352b0e0016febb8e03ec4c33ea8f721f
Brian Provenzano <bproven@gmail.com>
2018-10-29 21:35:57 -0600

    refactor: remove userdata (ansible handling apt upgrade/update via common role), add local-exec provisioner to call ansible, add tf var to control number of glusternodes managed by tf

--- glusterfs/main.tf
+++ glusterfs/main.tf
@@ -52,18 +52,18 @@ module "networking" {
 
 resource "aws_instance" "glusterfs" {
 
-  #create 3 instances for the cluster
-  count = 3
+  #number of nodes for the cluster
+  count = "${var.nodecount}"
 
-  connection={
-      user="ubuntu"
-      key_file="${var.key_file}"
-    }
+  # connection={
+  #     user="ubuntu"
+  #     key_file="${var.aws_keyfile}"
+  #   }
 
   ami = "${lookup(var.ubuntu1804_amis, var.region)}"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = "${var.aws_keyname}"
-  user_data = "${data.template_file.glusterfsnode_userdata.rendered}"
+  #user_data = "${data.template_file.glusterfsnode_userdata.rendered}"
   
   vpc_security_group_ids = ["${module.networking.ssh_security_group_id}"]
   subnet_id = "${module.networking.public_subnets[0]}"
@@ -74,4 +74,11 @@ resource "aws_instance" "glusterfs" {
     Environment = "${var.environment}"
     GlusterNode = "true"
   }
+
+  # Provision/configure the instances - have to sleep b/c we are using local-exec
+  # This allows the instances to fully start up.
+    provisioner "local-exec" {
+        working_dir = "../playbooks/"
+        command = "sleep 300; PRIVATEIPS=\"$(./ec2.py --profile default --list --refresh-cache | jq '._meta | {\"private_ips\":[.hostvars[].ec2_private_ip_address]}')\";ansible-playbook -b --private-key ${var.aws_keyfile} --limit ${self.public_ip} -e 'ansible_python_interpreter=/usr/bin/python3' --extra-vars 'ip=\"$${PRIVATEIPS}\"' gluster-cluster.yml"
+    }
 }
